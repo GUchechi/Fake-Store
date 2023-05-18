fe42af2cebc1ccccd1f5aeba9a9c40df776a8f53
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 20:18:45 +0100

    fetchMovieDetail

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "./MovieDetail.scss";
+import { useParams } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchAsyncMovieOrShowDetail,
+  getSelectedMovieOrShow,
+  // removeSelectedMovieOrShow,
+} from "../../features/movies/movieSlice";
+
 
 const MovieDetail = () => {
+  const { imdbID } = useParams();
+  const dispatch = useDispatch();
+  const data = useSelector(getSelectedMovieOrShow);
+
+  console.log(data)
+
+  useEffect(() => {
+    dispatch(fetchAsyncMovieOrShowDetail(imdbID));
+    // return () => {
+    //   dispatch(removeSelectedMovieOrShow());
+    // };
+  }, [dispatch, imdbID]);
   return ( 
     <div>MovieDetail</div>
   )
