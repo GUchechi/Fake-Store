4542f662d3e690419cc5b726f887940daf8b4696
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 17:17:46 +0100

    fetch Movies

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../apis/movieApi";
+import { APIKey } from "../../apis/MovieApiKey";
 
 const Home = () => {
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log("Err:", err);
+        });
+        console.log("THE response from API", response)
+    };
+    fetchMovies()
+  },[]);
   return (
     <div>
-      Home
+      <div className="banner-img"></div>
+      <MovieListing />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
