6e7d21ddef215b2f5cd5c7c9a85446f344e09d39
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 11:47:40 +0100

    productListing

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 const ProductListing = () => {
+    const products = useSelector((state) => state)
+    console.log(products)
   return (
     <div>
       Product Listing
