a4cf1c7fcbb988003d760ec3819aabdf3690c812
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 17:37:06 +0100

    fetch Movies

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../apis/movieApi";
 import { APIKey } from "../../apis/MovieApiKey";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const movieText = "Harry";
     const fetchMovies = async () => {
@@ -12,10 +16,10 @@ const Home = () => {
         .catch((err) => {
           console.log("Err:", err);
         });
-        console.log("THE response from API", response)
+        dispatch(addMovies(response.data))
     };
-    fetchMovies()
-  },[]);
+    fetchMovies();
+  }, []);
   return (
     <div>
       <div className="banner-img"></div>
