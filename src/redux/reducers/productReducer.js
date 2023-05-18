f5ea184eb5bc0ad40c918f5a89e5a77fd1d412c2
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 10:17:03 +0100

    product actions

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,10 @@
+import { ActionTypes } from "../contants/action-types";
+
+export const productReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+};
