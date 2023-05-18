37522b334e6c254c0c4df89b59b819d555e9f968
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 10:18:16 +0100

    product actions

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,5 +1,15 @@
 import { ActionTypes } from "../contants/action-types";
 
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "Godswill",
+      category: "programmer",
+    },
+  ],
+};
+
 export const productReducer = (state, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
