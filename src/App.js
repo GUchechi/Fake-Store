e3018fc87a25dd4dc1fa73ed1ecb28d416946353
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 16:39:56 +0100

    header

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import MovieDetail from "./components/MovieDetail/MovieDetail";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Header />
         <Route path="/" exact element={<Home />} />
         <Route path="/movie/:movieId" element={<MovieDetail />} />
         <Route element={<PageNotFound />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+    <div className="logo">Movie App</div>
+    <div className="user-image">
+      <img src={} alt="user" />
+    </div>
+    </div>
   )
 }
 
-export default Header
+export default Header 
