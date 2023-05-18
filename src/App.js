85c114325168dad2cdbacc219227d88adcf11c30
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 16:33:57 +0100

    movie js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Header />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/movie/:movieId" element={<MovieDetail />} />
+        <Route element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
