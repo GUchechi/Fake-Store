2d330a0ccb1c6ef7917417e3372d5f3d07127474
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 16:51:28 +0100

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import user from '../../images/user.png'
 
 const Header = () => {
   return (
     <div className='header'>
     <div className="logo">Movie App</div>
     <div className="user-image">
-      <img src={} alt="user" />
+      <img src={user} alt="user" />
     </div>
     </div>
   )
