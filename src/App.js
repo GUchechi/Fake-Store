999a66015fa3971c56e5dd76a372a47ec15b80f0
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 11:36:54 +0100

    productListing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProductListing />
     </div>
   );
 }
