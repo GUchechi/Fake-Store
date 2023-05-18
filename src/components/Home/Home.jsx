f42413779e85ace38618a8d6b2f4c901e42aea66
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 18:50:28 +0100

    fetchMovies

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,17 +6,12 @@ import { APIKey } from "../../apis/MovieApiKey";
 import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); 
 
   useEffect(() => {
     const movieText = "Harry";
     const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log("Err:", err);
-        });
-        dispatch(addMovies(response.data))
+    
     };
     fetchMovies();
   }, []);
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxj s/toolkit";
+
+const fetchAsyncMovies = createAsyncThunk('movies/fetchAsyncMovies', async () => {
+  const response = await movieApi
+  .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+  .catch((err) => {
+    console.log("Err:", err);
+  });
+  dispatch(addMovies(response.data))
+})
+
 
 const initialState = {
   movies: {},
@@ -6,7 +16,7 @@ const initialState = {
 
 const movieSlice = createSlice({
   name: "movies",
-  initialState,
+  initialState, 
   reducers: {
     addMovies: (state, { payload }) => {
       state.movies = payload;
