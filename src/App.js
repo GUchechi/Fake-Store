c6777dec95ee26a193f3d7175ed711bd3cf06aab
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 11:44:45 +0100

    productListing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./containers/Header";
-import ProductComponent from "./containers/ProductComponent";
 import ProductDetails from "./containers/ProductDetails";
 import ProductListing from "./containers/ProductListing";
 
@@ -8,9 +8,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <ProductListing />
-      <ProductComponent />
-      <ProductDetails />
+      <Routes>
+        <Route path="/" exact element={<ProductListing />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route>404 not Found</Route>
+      </Routes>
     </div>
   );
 }
