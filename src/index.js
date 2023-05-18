7449f70007819244469823f028dec836d3a9a16d
GUchechi <godswilluchendu7@gmail.com>
2023-05-18 17:22:36 +0100

    fetch Movies

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
+import {store} from "./features/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <Provider store={store}>
-  <Router>
-    <Routes>
-      <Route path="/*" element={<App />} />
-    </Routes>
-  </Router>
-  // </Provider>
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
