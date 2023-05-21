1457f5ccae6a0e2a39fef37403daae8856ca1d09
GUchechi <godswilluchendu7@gmail.com>
2023-05-21 19:00:16 +0100

    fetchAsyncMovies

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -52,7 +52,10 @@ const movieSlice = createSlice({
       .addCase(fetchAsyncMovies.pending, () => {
         console.log("Fetching movies...");
       })
-      
+      .addCase(fetchAsyncMovies.fulfilled, (state, { payload }) => {
+        console.log("Fetched movies successfully!");
+        state.movies = payload;
+      })
       .addCase(fetchAsyncMovies.rejected, () => {
         console.log("Failed to fetch movies!");
       })
