9d2cce644a5d578c209b05ae20630b1329155b35
GUchechi <godswilluchendu7@gmail.com>
2023-05-21 19:09:43 +0100

    missing page

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-const PageNotFound = () => {
+const Missing = () => {
   return (
-    <div>
-      PageNotFound
-    </div>
-  )
-}
+    <main className="Missing">
+      <h2>Page Not Found</h2>
+      <p>Well, that's disappointing.</p>
+      <p>
+        <Link to="/">Visit Our Homepage</Link>
+      </p>
+    </main>
+  );
+};
 
-export default PageNotFound
+export default Missing;
